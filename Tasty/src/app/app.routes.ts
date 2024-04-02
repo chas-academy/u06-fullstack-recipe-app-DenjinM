1121e5c0092fd4da4815fc60aade66b9fb287dd8
Denjin <denjin.muratagic@chasacademy.se>
2024-04-02 16:55:41 +0200

    added routes to all components

--- Tasty/src/app/app.routes.ts
+++ Tasty/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { SaladComponent } from './pages/salad/salad.component';
+import { SoupComponent } from './pages/soup/soup.component';
+import { SweetsComponent } from './pages/sweets/sweets.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { SearchComponent } from './pages/search/search.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'salad', component: SaladComponent },
+
+    { path: 'soup', component: SoupComponent },
+
+    { path: 'sweets', component: SweetsComponent },
+
+    { path: 'login', component: LoginComponent },
+
+    { path: 'register', component: RegisterComponent },
+
+    { path: 'search', component: SearchComponent },
+];
