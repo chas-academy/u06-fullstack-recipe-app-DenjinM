eb63d46fb564b8063dadf5dca25b643e611136a7
Denjin <denjin.muratagic@chasacademy.se>
2024-04-06 00:38:11 +0200

    Did some small changes in styling

--- Tasty/src/app/pages/home/home.component.ts
+++ Tasty/src/app/pages/home/home.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { RecipeService } from '../../services/recipe.service';
+import { Router } from '@angular/router';
+import { SearchComponent } from '../search/search.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [SearchComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  dishType = "Main course";
+  recipes?: any;
+  constructor(private recipeService: RecipeService, private router: Router) { }
+  recieveRecipes(recipes: any[]) {
+    this.recipes = recipes
+  }
+  goToDetails(recipe: any) {
+    let startIndex = recipe.self.indexOf("/v2/") + 4;
+    let endIndex = recipe.self.indexOf("?");
+    let extractedId = recipe.self.substring(startIndex, endIndex);
+    console.log(extractedId);
+    this.router.navigate(['/recipe', extractedId]);
+  }
 }
